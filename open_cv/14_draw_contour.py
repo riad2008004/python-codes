96cddbf053f67bbe75adc3660d8d7904895f012c
riad2008004 <arifulislamriad5@gmail.com>
2024-07-11 00:21:47 +0600

    contour

--- open_cv/14_draw_contour.py
+++ open_cv/14_draw_contour.py
@@ -5,15 +5,11 @@ sample_image_g=cv2.cvtColor(sample_image,cv2.COLOR_BGR2GRAY)
 _,binary_image=cv2.threshold(sample_image_g,200,255,cv2.THRESH_BINARY)
 contours,hc=cv2.findContours(binary_image,cv2.RETR_TREE,cv2.CHAIN_APPROX_NONE)
 
-cv2.drawContours(sample_image,contours,-1,(0,255,0),3)
+cv2.drawContours(sample_image,contours,5,(0,255,0),3)
 
 cv2.imshow("Original",sample_image)
 cv2.imshow("Gray",sample_image_g)
 cv2.imshow("Binary",binary_image)
-
-
-
-
 
 cv2.waitKey(0)
 cv2.destroyAllWindows()
