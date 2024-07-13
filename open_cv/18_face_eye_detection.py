b3cb5bcdd9186ca2320c68289c1283719247dba9
riad2008004 <arifulislamriad5@gmail.com>
2024-07-13 23:50:54 +0600

    face detection

---
+++ open_cv/18_face_eye_detection.py
@@ -0,0 +1,11 @@
+import cv2
+face_cascade=cv2.CascadeClassifier("xml_files/haarcascade_frontalface_default.xml")
+image=cv2.imread("images/riad.jpg")
+image_gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+faces=face_cascade.detectMultiScale(image_gray,1.1,4)
+
+for (x,y,w,h) in faces:
+    cv2.rectangle(image,(x,y),(x+w,y+h),(255,0,0),3)
+
+cv2.imshow("Image",image)
+cv2.waitKey(0)
