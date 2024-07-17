e6df8036cb869342e0971298535a3f2f5addcfef
riad2008004 <arifulislamriad5@gmail.com>
2024-07-17 17:07:22 +0600

    add button

--- tinkter/1_new_window.py
+++ tinkter/1_new_window.py
@@ -4,7 +4,13 @@ window = tk.Tk()
 window.title("Welcome")
 window.geometry("450x650")
 
-level_1=tk.Label(window,text="Welcome to my CONSOLE",bg='yellow',fg='green',font=("arial",16,'bold')).pack()
+level_1=tk.Label(window,text="Welcome to my CONSOLE",bg='yellow',fg='black',relief='solid',font=("arial",16,'bold'))
+level_1.pack()
+# level_1.place(x=100,y=100)
+button_1=tk.Button(window,text="Submit",bg='white',fg='green',relief='ridge',font=("arial",16,'bold'))
+button_1.place(x=330,y=600)
+
+
 
 window.mainloop()
 
