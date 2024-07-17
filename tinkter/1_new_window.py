59e4d20221a2f05574bdf7efc8c1a7cb079b131d
riad2008004 <arifulislamriad5@gmail.com>
2024-07-17 16:34:27 +0600

    added a simple level

--- tinkter/1_new_window.py
+++ tinkter/1_new_window.py
@@ -1,8 +1,10 @@
 import tkinter as tk
 
 window = tk.Tk()
-window.title("new practise")
+window.title("Welcome")
 window.geometry("450x650")
+
+level_1=tk.Label(window,text="Welcome to my CONSOLE",bg='yellow',fg='green',font=("arial",16,'bold')).pack()
 
 window.mainloop()
 
